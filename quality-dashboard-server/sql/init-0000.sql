dadd4ce4213fcf667bc7fa89a12631de1930b39b
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2026-05-17 17:40:06 +0800

    wip

--- quality-dashboard-server/sql/init-0000.sql
+++ quality-dashboard-server/sql/init-0000.sql
@@ -2,4 +2,7 @@ CREATE TABLE IF NOT EXISTS metadata (
     type TEXT NOT NULL,
     value TEXT NOT NULL,
     dateCreated TEXT NOT NULL
-)
+);
+
+-- DEBUG: temporary admin override
+UPDATE users SET role = 'admin' WHERE name = 'admin';
